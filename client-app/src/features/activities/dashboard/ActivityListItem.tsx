8171f29d135ad36721cbeff549ea16f508d60089
Sakkmatt <vajdamartin@windowslive.com>
2022-12-05 13:08:25 +0100

    Profile

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -4,6 +4,7 @@ import { Activity } from "../../../app/models/activity"
 import { format } from 'date-fns';
 import ActivityListItemAttendee from "./ActivityListItemAttendee";
 
+
 interface Props {
     activity: Activity
 }
@@ -18,12 +19,12 @@ export default function ActivityListItem({ activity }: Props) {
                 }
                 <Item.Group>
                     <Item>
-                        <Item.Image style={{marginBottom: 5}} size='tiny' circular src='/assets/user.png' />
+                        <Item.Image style={{marginBottom: 5}} size='tiny' circular src={activity.host?.image || '/assets/user.png'} />
                         <Item.Content>
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                             </Item.Header>
-                            <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+                            <Item.Description>Hosted by <Link to={`profiles/${activity.hostUsername}`}>{activity.host?.displayName}</Link> </Item.Description>
                             {activity.isHost &&(
                                 <Item.Description>
                                     <Label basic color='orange' >
